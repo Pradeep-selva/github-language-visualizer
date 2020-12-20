f9af201a1fcc235452c31c7569e2021901ecb8c2
pradeep-selva <pradeepselvaraju@gmail.com>
2020-12-20 21:07:09 +0530

    project styles setup

--- src/App.svelte
+++ src/App.svelte
@@ -8,21 +8,11 @@
 <style>
 	main {
 		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+		height: 100vh;
+		display: flex;
+		align-items: center;
+		justify-content: center;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+	
 </style>
